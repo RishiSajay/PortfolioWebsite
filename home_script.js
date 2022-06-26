dc2e5e1fbe74361d4740cdf2b14b9d09679016b1
RishiSajay <57403366+RishiSajay@users.noreply.github.com>
2022-06-26 12:57:01 -0400

    Add files via upload

--- home_script.js
+++ home_script.js
@@ -11,6 +11,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor =>
             
         });
 });
+/////////////////////////////////////////////////////////////////////////////////////
 
 //change navbar background based on position
 doc = document.documentElement;
@@ -50,6 +51,7 @@ window.addEventListener("scroll", function(event)
         navbar.classList.add('contact');
     }
 });
+/////////////////////////////////////////////////////////////////////////////////////
 
 //mobile navbar functionality
 const menu1 = document.querySelector('#mobile-menu');
@@ -60,7 +62,60 @@ menu1.addEventListener('click', function()
     menu1.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+/////////////////////////////////////////////////////////////////////////////////////
 
+
+//change background height based on lowest element location for each section. Useful to adjust to all screen sizes ie mobile, ipad, etc
+//intro
+intro = document.querySelector('.home_container');
+introCard = document.querySelector('.intro_card_container');
+
+//about me
+aboutMe = document.querySelector('.about_me_container');
+myPic = document.querySelector('.my_picture');
+
+//skills
+skills = document.querySelector('.skills_container');
+skillsBody = document.querySelector('.skills_body_container');
+
+//projects
+projects = document.querySelector('.projects_container');
+lastProject = document.querySelector('.projects_card:nth-child(5)');
+
+//contact
+contact = document.querySelector('.contact_me_container');
+message = document.querySelector('.message_container');
+
+resizeHeight = function()
+{
+  if(window.innerWidth <= 500)
+  {
+    height1 = (introCard.getBoundingClientRect().bottom) - (intro.getBoundingClientRect().top);
+    intro.style.height = height1 + "px";
+  }
+  else
+  {
+    height1 = (introCard.getBoundingClientRect().bottom + 200) - (intro.getBoundingClientRect().top);
+    intro.style.height = height1 + "px";
+  }
+
+  height2 = (myPic.getBoundingClientRect().bottom + 100) - (aboutMe.getBoundingClientRect().top);
+  aboutMe.style.height = height2 + "px";
+
+  height3 = (skillsBody.getBoundingClientRect().bottom + 200) - (skills.getBoundingClientRect().top);
+  skills.style.height = height3 + "px";
+
+  height4 = (lastProject.getBoundingClientRect().bottom + 100) - (projects.getBoundingClientRect().top);
+  projects.style.height = height4 + "px";
+
+  height5 = (message.getBoundingClientRect().bottom + 70) - (contact.getBoundingClientRect().top);
+  contact.style.height = height5 + "px";
+}
+
+
+window.onload = resizeHeight;
+window.addEventListener("resize", resizeHeight);
+/////////////////////////////////////////////////////////////////////////////////////
 
 //info card functionality
 const card = document.querySelector('.intro_card');
@@ -71,6 +126,7 @@ card.addEventListener('click', function()
     card.classList.toggle('active');
     text.classList.toggle('active');
 });
+/////////////////////////////////////////////////////////////////////////////////////
  
 
 
@@ -86,10 +142,11 @@ for (let i = 0; i < projectCards.length; i++)
         projectTexts[i].classList.toggle('active');
     });
 }
+/////////////////////////////////////////////////////////////////////////////////////
 
 //contact me 
-//This section removes the api success screen and puts my own
-//This was given by formspree api
+//The formspree api is used to generate a message and send it to my email after the user clicks submit
+//This section removes the api success screen and puts my own success messages
 
 var form = document.getElementById("my-form");
     
